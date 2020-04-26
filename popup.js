76e4e166f10b03e55b1b00feb78867f10df19c40
Sir Code <taylorschneider07@yahoo.com>
2020-04-26 12:25:40 -0400

    Fixed

---
+++ popup.js
@@ -0,0 +1,15 @@
+const toBase64 = (file) =>
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+    });
+async function Main() {
+    const file = document.querySelector("#myfile").files[0];
+    document.querySelector("#myoutput").innerText = await toBase64(file);
+}
+document.querySelector("#myfile").addEventListener("change", () => {
+    document.querySelector("#myoutput").innerText = "Converting... \n\n  It will take longer for larger files ;)";
+    setTimeout(Main, 100);
+});
