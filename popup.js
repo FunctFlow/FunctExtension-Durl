349410f55f0c975876f0bf50f30aab8d9c7c0dec
Sir Code <taylorschneider07@yahoo.com>
2020-05-01 02:28:59 -0400

    Major Final Changes and Full Rewrite. v2.0

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
+document.querySelector(".data#Output").setAttribute("placeholder", "Click Browse Files\n\nDataURL Will Output Here\n\nOnce Loaded, Click This Textarea To Copy The DataURL");
+document.querySelector(".data#Input").setAttribute("placeholder", "Paste DataURL Here\n\nThen Tap Download File\n\nIt Should Begin Shortly After\n\n\nTHIS IS EXPERIMENTAL\nYou need to type your own extension for the file when downloaded.");
+
 const toBase64 = (file) =>
     new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -6,11 +9,26 @@ const toBase64 = (file) =>
         reader.onerror = (error) => reject(error);
     });
 async function Main() {
-    const file = document.querySelector("#myfile").files[0];
-    document.querySelector("#myoutput").innerText = await toBase64(file);
+    const file = document.querySelector("#fileInput").files[0];
+    document.querySelector(".data#Output").innerText = await toBase64(file);
 }
 
-document.querySelector("#myfile").addEventListener("change", () => {
-    document.querySelector("#myoutput").innerText = "Converting...&#013;&#010;&#013;&#010;It will take longer for larger files ;)";
+document.querySelector("#fileInput").addEventListener("change", () => {
+    document.querySelector(".data#Output").innerText = "Converting...";
     setTimeout(Main, 100);
+});
+
+document.querySelector(".data#Output").addEventListener("click", (e) => {
+    e.target.select();
+    document.execCommand('copy');
+});
+
+document.querySelector(".urlButton").addEventListener("click", (e) => {
+    const el = document.createElement("a");
+    el.download = "file.ext";
+    el.href = document.querySelector(".data#Input").value;
+    el.style.display = "none";
+    document.body.appendChild(el);
+    el.click();
+    document.body.removeChild(el);
 });
